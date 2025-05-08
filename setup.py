aabd4d8c3ec008c234c5f54d1ef1e010dfeda83d
Kriti Kumari <kritikriti666555@gmail.com>
2025-05-08 20:46:39 +0530

    proj structure ready

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Kriti Kumari",
+    author_email="kritikriti555666@gmail.com",
+    packages=find_packages(),
+)
